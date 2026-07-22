32dc69810884c97d7a2f70a9471c287b4887f3df
Everton Emanuel <evertonemanuel1607@gmail.com>
2026-07-21 21:56:11 -0300

    Snake 2

--- Python/Snake.py
+++ Python/Snake.py
@@ -2,6 +2,7 @@ import time
 import keyboard
 
 points = 0
+d = "right"
 
 places = {
     "up" : ["0","0","0","0","0","0","0","0","0"],
@@ -11,15 +12,40 @@ places = {
 }
 
 def keyboard_read():
+    global d
     if keyboard.is_pressed("A"): # Preciso de uma manneira de manter o resultado da última aplicação, para assim ele posso continuar percorrer
         d = "left"
     elif keyboard.is_pressed("D"):
         d = "right"
+    elif keyboard.is_pressed("W"):
+        d = "up"
+    elif keyboard.is_pressed("S"):
+        d = "down"
     return d
 
 def show_places():
     for p in places:
         print(" ".join(places[p]))
+
+def where_up(postion):
+    if postion == "down":
+        return "mid2"
+    elif postion == "mid2":
+        return "mid"
+    elif postion == "mid":
+        return "up"
+    elif postion == "up":
+        return "down"
+    
+def where_down(postion):
+    if postion == "down":
+        return "up"
+    elif postion == "mid2":
+        return "down"
+    elif postion == "mid":
+        return "mid2"
+    elif postion == "up":
+        return "mid"
 
 def walk():
     for p in places:
@@ -34,6 +60,18 @@ def walk():
                 if places[p][ind - 1] == "0":
                     places[p][ind] = "0"
                     places[p][ind - 1] = "1"
+            elif "1" in places[p] and keyboard_read() == "up":
+                ind = places[p].index("1")
+                if places[where_up(p)][ind] == "0":
+                    places[p][ind] = "0"
+                    places[where_up(p)][ind] = "1"
+            elif "1" in places[p] and keyboard_read() == "down":
+                ind = places[p].index("1")
+                if places[where_down(p)][ind] == "0":
+                    places[p][ind] = "0"
+                    places[where_down(p)][ind] = "1"
+                    
+            
         except IndexError:
             places[p][0] = "1"
             places[p][ind] = "0"
@@ -43,12 +81,13 @@ def walk():
 print("=== Snake ===\n1- Start?\n2- Points")
 options = input("> ")
 if options == "1":
-    places["mid2"][4] = "1"
+    places["down"][4] = "1"
     while True:
         show_places()
         print("_____________________________")
+        time.sleep(1.5)
         walk()
-        time.sleep(1.2)
+        
 
     
 
